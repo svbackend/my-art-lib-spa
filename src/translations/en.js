3babb3a00ccc1629627e84216b6f340184832d65
Valentin <svbackend22@gmail.com>
2018-05-24 16:07:24 +0300

    Translations for english

--- src/translations/en.js
+++ src/translations/en.js
@@ -1,5 +1,17 @@
 export default {
   message: {
-    hello: 'hello world'
-  }
+    hello: 'Hello, world!'
+  },
+  validation: {
+    required: 'This field is required!',
+    minLength: '{fieldName} must be at least {min, plural, =1{1 character} other{# characters}} long',
+    _incorrect: 'Username or Password is incorrect, try again',
+  },
+  fields: {
+    username: 'Username',
+    password: 'Password',
+  },
+  loginPage: {
+    authorize: 'Sign In',
+  },
 }
