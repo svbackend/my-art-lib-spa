be93721e6ed20a0b886fe6364390e199294fb9a1
Valentin <svbackend22@gmail.com>
2018-06-02 13:54:35 +0300

    VS-master Route for registration

--- src/extensions/router.js
+++ src/extensions/router.js
@@ -4,6 +4,7 @@ import HomePage from './../pages/HomePage.vue'
 import NotFoundPage from './../pages/NotFoundPage.vue'
 import MoviePage from './../movies/pages/MoviePage.vue'
 import LoginPage from './../users/pages/LoginPage.vue'
+import RegistrationPage from './../users/pages/RegistrationPage.vue'
 
 export default new Router({
   routes: [
@@ -24,6 +25,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: LoginPage,
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: RegistrationPage,
     },
     {
       path: '/movie/:id',
