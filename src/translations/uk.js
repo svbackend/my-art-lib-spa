6b82b944ffbb1ae75a7caf93e3b865bdcdd53508
Valentin <svbackend22@gmail.com>
2018-06-02 13:53:35 +0300

    VS-master translations for registration page

--- src/translations/uk.js
+++ src/translations/uk.js
@@ -5,14 +5,22 @@ export default {
   validation: {
     required: 'Це поле є обов\'язковим!',
     minLength: 'Поле "{fieldName}" занадто коротке, мінімум {min, plural, =0{# символів} =1{1 символ} few{# символи} other{# символів}}!',
+    email: 'Введіть коректний емейл адрес',
+    validUsername: 'Логін повинен складатись лише з букв, цифр та символів: -, _',
     _incorrect: 'Невірний Логін або Пароль, попробуйте ще раз',
+    _isUniqueError: 'Такий {fieldName} вже зареєстрований'
   },
   fields: {
     username: 'Логін',
+    email: 'Email',
+    usernamePlaceholder: 'Іван_Іванович-95',
     password: 'Пароль',
   },
   loginPage: {
     authorize: 'Авторизуватись',
+  },
+  registrationPage: {
+    register: 'Зареєструватись',
   },
   movie: {
     addToWatchedMovies: 'Додати в колекцію',
