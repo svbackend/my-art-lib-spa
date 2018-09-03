21f7584d6a57049f61965f33327e1e3931897e42
Valentin <svbackend22@gmail.com>
2018-09-03 17:04:28 +0300

    Deployer setup

--- deploy.php
+++ deploy.php
@@ -2,11 +2,51 @@
 
 namespace Deployer;
 
-require_once 'recipe/common.php';
+// vendor/bin/dep deploy
 
-/*
-host('159.89.14.82')
-    ->user('svbackend')
-    ->identityFile('~/.ssh/dokey')
-    ->set('deploy_path', '~/{{application}}');
-*/
+require_once 'recipe/rsync.php';
+$config = require_once './deploy_config.php';
+
+set('rsync_src', __DIR__);
+set('release_path', $config['deploy_path']);
+
+host($config['host'])
+    ->user($config['login'])
+    ->set('deploy_path', $config['deploy_path']);
+
+set('rsync', [
+    'exclude' => [
+        '.node_modules',
+        '.git',
+        'deploy.php',
+        'deploy_config.php',
+        'old_index.html',
+    ],
+    'exclude-file' => false,
+    'include' => [],
+    'include-file' => false,
+    'filter' => [],
+    'filter-file' => false,
+    'filter-perdir' => false,
+    'flags' => 'rz',
+    'options' => ['delete'],
+    'timeout' => 300,
+]);
+
+task('deploy:prepare', function () {
+    $id = time();
+    runLocally("cp index.html old_index.html");
+    runLocally("sed -i 's/build.js/build.js\?v={$id}/g' index.html");
+    runLocally('npm run build');
+});
+
+task('cleanup', function () {
+    runLocally("unlink index.html");
+    runLocally("mv old_index.html index.html");
+});
+
+task('deploy', [
+    'deploy:prepare',
+    'rsync',
+    'cleanup',
+]);
