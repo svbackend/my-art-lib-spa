dedba97c76ca4d875c11fbeda1c7869725decc49
Valentin <svbackend22@gmail.com>
2018-09-03 09:19:04 +0300

    Deployer init config

---
+++ deploy.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Deployer;
+
+require_once 'recipe/common.php';
+
+/*
+host('159.89.14.82')
+    ->user('svbackend')
+    ->identityFile('~/.ssh/dokey')
+    ->set('deploy_path', '~/{{application}}');
+*/
