feba65310f20b2936fb526d097a259d07039a8fd
Valentin <svbackend22@gmail.com>
2018-09-04 19:52:26 +0300

    Fix redirect after user authorization

--- src/users/pages/LoginPage.vue
+++ src/users/pages/LoginPage.vue
@@ -3,44 +3,60 @@
     <div class="field">
       <label class="label has-text-left" v-t="'fields.username'"></label>
       <p class="control has-icons-left has-icons-right">
-        <input autofocus="autofocus" class="input" :class="{ 'is-success': $validator.isDirty('username') && !$validator.hasAnyError('username'), 'is-danger': $validator.isDirty('username') && $validator.hasAnyError('username') }" type="text" placeholder="Email or username" v-model.trim="username" @blur="$v.username.$touch()">
+        <input autofocus="autofocus" class="input"
+               :class="{ 'is-success': $validator.isDirty('username') && !$validator.hasAnyError('username'), 'is-danger': $validator.isDirty('username') && $validator.hasAnyError('username') }"
+               type="text" placeholder="Email or username" v-model.trim="username" @blur="$v.username.$touch()">
         <span class="icon is-small is-left">
           <i class="fa fa-envelope"></i>
         </span>
         <span class="icon is-small is-right">
-          <i v-if="$validator.isDirty('username') && $validator.hasAnyError('username')" class="fa fa-times has-text-danger"></i>
-          <i v-if="$validator.isDirty('username') && !$validator.hasAnyError('username')" class="fa fa-check has-text-success"></i>
+          <i v-if="$validator.isDirty('username') && $validator.hasAnyError('username')"
+             class="fa fa-times has-text-danger"></i>
+          <i v-if="$validator.isDirty('username') && !$validator.hasAnyError('username')"
+             class="fa fa-check has-text-success"></i>
         </span>
       </p>
       <p v-if="$validator.isDirty('username') && $validator.hasAnyError('username')" class="help is-danger">
-        <span v-if="$validator.hasError('username', 'required')" v-t="$validator.showError('username', 'required')"></span>
-        <span v-if="$validator.hasError('username', 'minLength')" v-t="$validator.showError('username', 'minLength')"></span>
+        <span v-if="$validator.hasError('username', 'required')"
+              v-t="$validator.showError('username', 'required')"></span>
+        <span v-if="$validator.hasError('username', 'minLength')"
+              v-t="$validator.showError('username', 'minLength')"></span>
       </p>
     </div>
 
     <label class="label has-text-left" v-t="'fields.password'"></label>
     <div class="field has-addons">
       <p class="control is-expanded has-icons-left has-icons-right">
-        <input class="input" :class="{ 'is-success': $validator.isDirty('password') && !$validator.hasAnyError('password'), 'is-danger': $validator.isDirty('password') && $validator.hasAnyError('password') }" :type="passwordInputType" placeholder="Password" v-model.trim="password" @blur="$v.password.$touch()">
+        <input class="input"
+               :class="{ 'is-success': $validator.isDirty('password') && !$validator.hasAnyError('password'), 'is-danger': $validator.isDirty('password') && $validator.hasAnyError('password') }"
+               :type="passwordInputType" placeholder="Password" v-model.trim="password" @blur="$v.password.$touch()">
         <span class="icon is-small is-left">
           <i class="fa fa-lock"></i>
         </span>
         <span class="icon is-small is-right">
-          <i v-if="$validator.isDirty('password') && $validator.hasAnyError('password')" class="fa fa-times has-text-danger"></i>
-          <i v-if="$validator.isDirty('password') && !$validator.hasAnyError('password')" class="fa fa-check has-text-success"></i>
+          <i v-if="$validator.isDirty('password') && $validator.hasAnyError('password')"
+             class="fa fa-times has-text-danger"></i>
+          <i v-if="$validator.isDirty('password') && !$validator.hasAnyError('password')"
+             class="fa fa-check has-text-success"></i>
         </span>
       </p>
 
       <p class="control">
-        <a class="button" :class="{'is-dark': this.passwordInputType === 'password', 'is-black': this.passwordInputType === 'text'}" @click="switchPasswordInputType">
+        <a class="button"
+           :class="{'is-dark': this.passwordInputType === 'password', 'is-black': this.passwordInputType === 'text'}"
+           @click="switchPasswordInputType">
           <i class="fa fa-eye"></i>
         </a>
       </p>
     </div>
-    <p v-if="$validator.isDirty('password') && $validator.hasAnyError('password')" class="help help-underField is-danger">
-      <span v-if="$validator.hasError('password', 'required')" v-t="$validator.showError('password', 'required')"></span>
-      <span v-if="$validator.hasError('password', 'minLength')" v-t="$validator.showError('password', 'minLength')"></span>
-      <span v-if="$validator.hasError('password', '_incorrect')" v-t="$validator.showError('password', '_incorrect')"></span>
+    <p v-if="$validator.isDirty('password') && $validator.hasAnyError('password')"
+       class="help help-underField is-danger">
+      <span v-if="$validator.hasError('password', 'required')"
+            v-t="$validator.showError('password', 'required')"></span>
+      <span v-if="$validator.hasError('password', 'minLength')"
+            v-t="$validator.showError('password', 'minLength')"></span>
+      <span v-if="$validator.hasError('password', '_incorrect')"
+            v-t="$validator.showError('password', '_incorrect')"></span>
     </p>
 
     <p class="control">
@@ -96,28 +112,33 @@
             username: this.username,
             password: this.password,
           }
-        }).then(response => {
-          this.$validator.clearErrors();
-          this.$v.$touch()
-          this.submitStatus = 'OK'
-          this.signIn(response.data.api_token, response.data.user_id);
-        }).catch(error => {
-          if (error.response.status === 500) {
-            return this.showServerError();
-          }
-          return this.showErrors(error.response.data);
         })
+          .then(response => {
+            this.$validator.clearErrors();
+            this.$v.$touch()
+            this.submitStatus = 'OK'
+            this.signIn(response.data.api_token, response.data.user_id);
+            this.$router.push({name: 'home'})
+          })
+          .catch(error => {
+            if (!error.response) {
+              return;
+            }
+            if (error.response.status === 500) {
+              return this.showServerError();
+            }
+            return this.showErrors(error.response.data);
+          })
       },
       signIn(apiToken, userId) {
         this.$store.dispatch('setApiToken', apiToken)
         this.saveUser(userId)
-        this.$router.push({name: 'home'})
       },
       saveUser(userId) {
         this.$http.get('/users/' + userId)
           .then(response => {
             this.$store.dispatch('setUser', response.data)
-        })
+          })
       },
       showErrors(errors) {
         this.submitStatus = ''
