999808e61e4449fef337727e159618af5c7f74db
Valentyn Saik <svbackend22@gmail.com>
2018-09-30 16:55:29 +0200

    Movies library -- now other users can see your movie ratings

--- src/movies/components/movie.vue
+++ src/movies/components/movie.vue
@@ -46,6 +46,9 @@
             :to="{ name: 'movie', params: { id: movie.id } }">
           {{ movie.title }} ({{ movie.releaseDate | year }})
         </router-link>
+        <div class="owner-rating" v-if="movie.ownerWatchedMovie && movie.ownerWatchedMovie.vote > 0">
+            {{ movie.ownerWatchedMovie.vote }}<span class="icon"><i class="fa fa-star has-text-orange"></i></span>
+        </div>
       </div>
     </div>
   </div>
